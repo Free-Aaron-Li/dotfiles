372c341d970755ddea4e8869f30b30ac4972947e
aaron <95997167+Free-Aaron-Li@users.noreply.github.com>
2024-01-21 18:01:56 +0800

    🎼  improvement(schema): modifying the schema

--- nvim/lua/plugins/plugin-theme.lua
+++ nvim/lua/plugins/plugin-theme.lua
@@ -1,14 +1,22 @@
 return{
 	{
-		 "neanias/everforest-nvim",
-		 version=false,
-		 lazy=false,
-		 pririty=1000,
-		 config=function()
-			 require("everforest").setup({
+		"neanias/everforest-nvim",
+		version=false,
+		lazy=false,
+		pririty=1000,
+		config=function()
+			require("everforest").setup({
 
-			 })
-		 end,
+			})
+		end,
 	}, 
+	{
+		"shaunsingh/nord.nvim",
+		version="*",
+		lazy = false,
+		priority = 1000,
+		config = function()
+		end,
+	},
 } 
 
